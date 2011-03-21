db1612786846aeaad59032d01b2d571bdf4baee5
lucapette <lucapette@gmail.com>
2011-03-21 23:14:34 +0100

    initial commit for status line features

--- ruby/codepath.rb
+++ ruby/codepath.rb
@@ -7,12 +7,12 @@ class CodePath
     end
 
     def codedir(current_dir)
+        code_dir="" 
         if codedir?(current_dir)
             current_dir[/#{@code_dir}/]=""
-            "#{@code_dir}/#{current_dir.match(/^\/([^\/]+)/)[1]}"
-        else
-            current_dir
+            code_dir="#{@code_dir}/#{current_dir.match(/^\/([^\/]+)/)[1]}"
         end
+        code_dir
     end
 
     def subdirs(path)
@@ -21,6 +21,24 @@ class CodePath
 
     def codedir?(current_dir)
         current_dir.include?(@code_dir) && current_dir != @code_dir
+    end
+
+    def project_name(current_file)
+        project = ""
+        if codedir?(File.dirname(current_file))
+            current_file[@code_dir]=""
+            project=current_file.split("/")[1]
+        end
+        project
+    end
+
+    def file_name(current_file)
+        file = current_file
+        if codedir?(File.dirname(current_file))
+            current_file["#{@code_dir}/#{project_name(current_file)}"]=""
+            file = current_file
+        end
+        file
     end
 
 end
