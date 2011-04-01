6d1d63e72cf9a24aa4b3661a8e9bee8d4499542c
lucapette <lucapette@gmail.com>
2011-04-01 18:00:19 +0200

    rspec initial refactoring

--- spec/codepath_spec.rb
+++ spec/codepath_spec.rb
@@ -1,21 +1,25 @@
 require File.expand_path("../../ruby/codepath",__FILE__)
 
 describe CodePath do
-    let(:codepath) {CodePath.new("/home/lucapette/code")}
-    let(:good_dir) {"/home/lucapette/code/project/subdir"}
-    let(:bad_dir) {"/home/lucapette/notcode/project/subdir"}
+
+    before(:each) do
+        @codepath = CodePath.new("/home/lucapette/code")
+        @good_dir = "/home/lucapette/code/project/subdir"
+        @bad_dir = "/home/lucapette/notcode/project/subdir"
+
+    end
 
     describe "#codedir?" do
 
         context "with a dir in path" do
             it "returns true" do
-                codepath.codedir?(good_dir).should be true
+                @codepath.codedir?(@good_dir).should be true
             end
         end
 
         context "with a dir not in path" do
             it "returns false" do
-                codepath.codedir?(bad_dir).should be false
+                @codepath.codedir?(@bad_dir).should be false
             end
         end
 
@@ -24,12 +28,12 @@ describe CodePath do
     describe "#codedir" do
         context "with a dir in path" do
             it "returns the current root project dir" do
-                codepath.codedir(good_dir).should == "/home/lucapette/code/project"
+                @codepath.codedir(@good_dir).should == "/home/lucapette/code/project"
             end
         end
         context "with a dir not in path" do
             it "returns the dir itself" do
-                codepath.codedir(bad_dir).should == bad_dir
+                @codepath.codedir(@bad_dir).should == @bad_dir
             end
         end
     end
